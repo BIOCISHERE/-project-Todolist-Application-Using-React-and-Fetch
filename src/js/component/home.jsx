b4c05ddf649974698884142077edc6558c858997
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-29 17:28:28 +0000

    falta agregar el fetch de agregar y eliminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,78 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [listTodods, setListTodos] = useState([]);
+
+	const get = () => {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/BIOCISHERE')
+		.then(res => {
+			if(res.status >= 200 && res.status < 300){
+				console.log('request success');
+				return res.json();
+			}
+		})
+		.then(data => setListTodos(data))
+		.catch(error => console.error('Error:', error))
+	}
+
+	useEffect(() => {
+		get()
+	}, [])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<div className="container">
+			<h1 className="text-center">List of my todos</h1>
+			<p className="text-center">
+				Click on the input, add your todo and press enter
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+			<p className="text-center">
+				Then to delete, hover on a task and click on the trashcan icon
 			</p>
+			<div className="border border-secondary rounded p-1">
+				<ul className="p-0 mx-0">
+					<li className="border-bottom p-1">
+						<input
+							type="text"
+							placeholder="What needs to be done?"
+							className="w-100 border-0"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyUp={(e) => {
+								if (e.key === "Enter") {
+									if (inputValue === "") {
+										alert("No tasks, add a task");
+									} else {
+										setListTodos(listTodods.concat(inputValue));
+										setInputValue("");
+									}
+								}
+							}}
+						/>
+					</li>
+					{listTodods.map((todo, index) => (
+						<li key={index} className="showAndHide border-bottom my-1 p-1">
+							{todo.label}
+							<i
+								className="fas fa-trash-alt float-end py-1 pe-2"
+								onClick={() =>
+									setListTodos(
+										listTodods.filter(
+											(todo, currentIndex) => index != currentIndex
+										)
+									)
+								}
+							></i>
+						</li>
+					))}
+				</ul>
+				<h4 className="text-center">{listTodods.length} tasks left</h4>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+// https://assets.breatheco.de/apis/fake/todos/user/85
+// https://assets.breatheco.de/apis/fake/todos/user/BIOCISHERE
