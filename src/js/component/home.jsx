ec3aa1b932347b0f36800756170c144f1354d3cf
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-30 19:58:51 +0000

    esta listo pero no funciona de la manera correcta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,29 +6,43 @@ const Home = () => {
 	const [listTodos, setListTodos] = useState([]);
 
 	const get = () => {
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/BIOCISHERE')
-		.then(res => {
-			if(res.status >= 200 && res.status < 300){
-				console.log('request success');
-				return res.json();
-			}
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/biocishere')
+		.then(res => res.json())
+		.then(data => setListTodos(...listTodos, data))
+	}
+
+	const put = (todo) => {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/biocishere', {
+			method: "PUT",
+            body: JSON.stringify(todo),
+            headers: {
+                "Content-Type": "application/json"
+            }
 		})
-		.then(data => setListTodos(data))
-		.catch(error => console.error('Error:', error))
+		.then(res => res.json())
+		.then(data => console.log(data))
 	}
 
 	useEffect(() => {
 		get();
 	}, [])
 
+	
+	useEffect(() => {
+		put(listTodos);	
+	}, [listTodos])
+
 	return (
-		<div className="container">
+		<div className="container mt-2">
 			<h1 className="text-center">List of my todos</h1>
 			<p className="text-center">
-				Click on the input, add your todo and press enter
+				Click on the input, add your todo and press enter.
 			</p>
 			<p className="text-center">
-				Then to delete, hover on a task and click on the trashcan icon
+				Then to delete, hover on a task and click on the trashcan icon.
+			</p>
+			<p className="text-center">
+				If you have no tasks, then when you reload the page it'll show your last added task.
 			</p>
 			<div className="border border-secondary rounded p-1">
 				<ul className="p-0 mx-0">
@@ -44,10 +58,11 @@ const Home = () => {
 									if (inputValue === "") {
 										alert("No tasks, add a task");
 									} else {
-										setListTodos(listTodos.concat({'label': inputValue, 'done': false}));
+										setListTodos(listTodos.concat( { "label": inputValue, "done": false } ));
 										setInputValue("");
-									}
-								}
+										put(listTodos);
+									};
+								};
 							}}
 						/>
 					</li>
@@ -56,18 +71,23 @@ const Home = () => {
 							{todo.label}
 							<i
 								className="fas fa-trash-alt float-end py-1 pe-2"
-								onClick={() =>
+								onClick={() =>{
+									put(listTodos),
 									setListTodos(
 										listTodos.filter(
-											(todo, currentIndex) => index != currentIndex
+											(todo, currentIndex) => (index != currentIndex)
 										)
-									)
+									)}
 								}
 							></i>
 						</li>
 					))}
 				</ul>
 				<h4 className="text-center">{listTodos.length} tasks left</h4>
+				<div className="text-center m-1">
+				    <button type="button" className="btn btn-danger my-2" onClick={() => setListTodos([])}>Delete all tasks</button> <br />
+					<span>After clicking the button, create a new task to save changes.</span>
+				</div>
 			</div>
 		</div>
 	);
